229aa51f49a24836ddfbd6a1da2ee0f24170835e
Anton Voytov <august-ra@ya.ru>
2024-10-17 23:55:38 +0300

    Fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-import AppRoutes from "./AppRoutes"
+import App from "./App"
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRoutes />
+    <App />
   </StrictMode>,
 )
